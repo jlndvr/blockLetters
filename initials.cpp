9fbdc9b0758d49cfb9c545093e81d99e4af0cb07
Josué Obed Landaverde <129899297+jlndvr@users.noreply.github.com>
2025-05-08 14:32:33 -0500

    Create initials.cpp

---
+++ initials.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+  // I am 5'10
+  cout<< "    A    B\n";
+  cout<< "    A    B\n";
+  cout<< "    A    B\n";
+  cout<< "A   A    B\n";
+  cout<< " A A     B B B\n";
+
+}
